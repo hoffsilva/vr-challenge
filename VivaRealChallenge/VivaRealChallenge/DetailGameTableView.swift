3be13b38c5bb6419a0fa3d4014525c6dc8157df1
Hoff Silva <hoff.henry@gmail.com>
2017-08-02 16:47:53 -0300

    Code review.

--- VivaRealChallenge/VivaRealChallenge/DetailGameTableView.swift
+++ VivaRealChallenge/VivaRealChallenge/DetailGameTableView.swift
@@ -31,20 +31,13 @@ class DetailGameTableView: UITableViewController {
         loadDataGame()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
         return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
         return 8
     }
     
@@ -65,7 +58,6 @@ class DetailGameTableView: UITableViewController {
     
     @IBAction func closeView() {
         let vc = self.storyboard!.instantiateViewController(withIdentifier: "topGames")
-        // the following two lines configures the animation. default is .auto
         Hero.shared.setDefaultAnimationForNextTransition(HeroDefaultAnimationType.zoomOut)
         Hero.shared.setContainerColorForNextTransition(.lightGray)
         hero_replaceViewController(with: vc)
--- VivaRealChallenge/VivaRealChallenge/GameCollectionView.swift
+++ VivaRealChallenge/VivaRealChallenge/GameCollectionView.swift
@@ -26,27 +26,18 @@ class GameCollectionView: UICollectionViewController {
         controller.delegate = self
         pleaseWait()
         controller.getGames(quantity: 50)
-        
         collectionView?.backgroundView = UIImageView(image: #imageLiteral(resourceName: "ac.jpg"))
         collectionView?.backgroundView?.alpha = 0.4
-
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
     // MARK: UICollectionViewDataSource
 
     override func numberOfSections(in collectionView: UICollectionView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
         return 1
     }
 
 
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of items
         return controller.arrayOfGames.count
     }
 
@@ -64,51 +55,14 @@ class GameCollectionView: UICollectionViewController {
     override func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
         cell.backgroundColor = UIColor.clear
     }
-    
-    
 
     override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        
         controller.detailGame(rowOfGame: indexPath.row)
-        
         let vc = self.storyboard!.instantiateViewController(withIdentifier: "detailGame")
-        
-        // the following two lines configures the animation. default is .auto
         Hero.shared.setDefaultAnimationForNextTransition(HeroDefaultAnimationType.zoom)
         Hero.shared.setContainerColorForNextTransition(.lightGray)
-        
         hero_replaceViewController(with: vc)
     }
-
-    
-    /*
-    // Uncomment this method to specify if the specified item should be highlighted during tracking
-    override func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool {
-        return true
-    }
-    */
-
-    /*
-    // Uncomment this method to specify if the specified item should be selected
-    override func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
-        return true
-    }
-    */
-
-    /*
-    // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
-    override func collectionView(_ collectionView: UICollectionView, shouldShowMenuForItemAt indexPath: IndexPath) -> Bool {
-        return false
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, canPerformAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) -> Bool {
-        return false
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, performAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) {
-    
-    }
-    */
 
 }
 
